ab6bc248a950491ffa20db2a9aa23cef188c3061
tomigorn <tomigorn@gmail.com>
2025-12-25 21:08:27 +0100

    integrate MSAL for Azure authentication

--- SBB-EasyRide-TaxReport/frontend/sbb-easyridetaxreport-web/src/main.tsx
+++ SBB-EasyRide-TaxReport/frontend/sbb-easyridetaxreport-web/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./authConfig";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const msalInstance = new PublicClientApplication(msalConfig);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
+  </React.StrictMode>
+);

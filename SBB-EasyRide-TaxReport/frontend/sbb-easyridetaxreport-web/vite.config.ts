69d2b88e47cda39b4f16242396cd025f8c5d8c3b
tomigorn <tomigorn@gmail.com>
2025-12-25 21:08:09 +0100

    configure webapi location for frontend

--- SBB-EasyRide-TaxReport/frontend/sbb-easyridetaxreport-web/vite.config.ts
+++ SBB-EasyRide-TaxReport/frontend/sbb-easyridetaxreport-web/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), mkcert()],
+  server: {
+    port: 5173,
+    https: true as any,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5145',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
